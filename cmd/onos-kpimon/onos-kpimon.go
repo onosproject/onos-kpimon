6fcc20443e28cbb3af9174876adf6b4cde0917bb
Woojoong Kim <woojoong.m.kim@gmail.com>
2020-12-11 10:30:54 -0800

    Remove hardcoded grpc port number

--- cmd/onos-kpimon/onos-kpimon.go
+++ cmd/onos-kpimon/onos-kpimon.go
@@ -23,6 +23,7 @@ func main() {
 	e2tEndpoint := flag.String("e2tEndpoint", "onos-e2t:5150", "E2T service endpoint")
 	e2subEndpoint := flag.String("e2subEndpoint", "onos-e2sub:5150", "E2Sub service endpoint")
 	ricActionID := flag.Int("ricActionID", 10, "RIC Action ID in E2 message")
+	grpcPort := flag.Int("grpcPort", 5150, "grpc Port number")
 
 	ready := make(chan bool)
 
@@ -47,7 +48,7 @@ func main() {
 		CertPath:      *certPath,
 		E2tEndpoint:   *e2tEndpoint,
 		E2SubEndpoint: *e2subEndpoint,
-		GRPCPort:      5150,
+		GRPCPort:      *grpcPort,
 		GnmiConfig:    &gnmiConfig,
 		RicActionID:   int32(*ricActionID),
 	}
