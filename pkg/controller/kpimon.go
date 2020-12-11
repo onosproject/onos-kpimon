5a10a0e482aece03054cb34c6e4c9531ed109258
Woojoong Kim <woojoong.m.kim@gmail.com>
2020-12-11 10:18:09 -0800

    Change import proto repo

--- pkg/controller/kpimon.go
+++ pkg/controller/kpimon.go
@@ -6,7 +6,7 @@ package controller
 
 import (
 	"fmt"
-	"github.com/golang/protobuf/proto"
+	"google.golang.org/protobuf/proto"
 	e2sm_kpm_ies "github.com/onosproject/onos-e2-sm/servicemodels/e2sm_kpm/v1beta1/e2sm-kpm-ies"
 	"github.com/onosproject/onos-lib-go/pkg/logging"
 	"github.com/onosproject/onos-ric-sdk-go/pkg/e2/indication"
