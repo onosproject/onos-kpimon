9603b692d1d6a32d7588fae67f4e8a22de6c07ce
adibrastegarnia <adib.rastegarnia@gmail.com>
2021-06-16 16:02:27 -0700

    Fix defining reduandant action  defintions

--- pkg/southbound/e2/subscription/utils.go
+++ pkg/southbound/e2/subscription/utils.go
@@ -8,11 +8,12 @@ import (
 	"context"
 
 	"github.com/google/uuid"
+	actionsstore "github.com/onosproject/onos-kpimon/pkg/store/actions"
+
 	e2api "github.com/onosproject/onos-api/go/onos/e2t/e2/v1beta1"
 	topoapi "github.com/onosproject/onos-api/go/onos/topo"
 	"github.com/onosproject/onos-e2-sm/servicemodels/e2sm_kpm_v2/pdubuilder"
 	e2smkpmv2 "github.com/onosproject/onos-e2-sm/servicemodels/e2sm_kpm_v2/v2/e2sm-kpm-v2"
-	actionsstore "github.com/onosproject/onos-kpimon/pkg/store/actions"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -36,45 +37,44 @@ func (m *Manager) createSubscriptionActions(ctx context.Context, reportStyle *to
 				return nil, err
 			}
 			measInfoList.Value = append(measInfoList.Value, meanInfoItem)
-
-			subID := int64(uuid.New().ID())
-			actionDefinition, err := pdubuilder.CreateActionDefinitionFormat1(cell.GetCellObjectID(), measInfoList, granularity, subID)
-			if err != nil {
-				return nil, err
-			}
-
-			key := actionsstore.NewKey(actionsstore.SubscriptionID{
-				SubID: subID,
-			})
-			// TODO clean up this store if we delete subscriptions
-			_, err = m.actionStore.Put(ctx, key, actionDefinition)
-			if err != nil {
-				log.Warn(err)
-				return nil, err
-			}
-
-			e2smKpmActionDefinition, err := pdubuilder.CreateE2SmKpmActionDefinitionFormat1(reportStyle.Type, actionDefinition)
-			if err != nil {
-				return nil, err
-			}
-
-			e2smKpmActionDefinitionProto, err := proto.Marshal(e2smKpmActionDefinition)
-			if err != nil {
-				return nil, err
-			}
-
-			action := &e2api.Action{
-				ID:   int32(index),
-				Type: e2api.ActionType_ACTION_TYPE_REPORT,
-				SubsequentAction: &e2api.SubsequentAction{
-					Type:       e2api.SubsequentActionType_SUBSEQUENT_ACTION_TYPE_CONTINUE,
-					TimeToWait: e2api.TimeToWait_TIME_TO_WAIT_ZERO,
-				},
-				Payload: e2smKpmActionDefinitionProto,
-			}
-
-			actions = append(actions, *action)
 		}
+		subID := int64(uuid.New().ID())
+		actionDefinition, err := pdubuilder.CreateActionDefinitionFormat1(cell.GetCellObjectID(), measInfoList, granularity, subID)
+		if err != nil {
+			return nil, err
+		}
+
+		key := actionsstore.NewKey(actionsstore.SubscriptionID{
+			SubID: subID,
+		})
+		// TODO clean up this store if we delete subscriptions
+		_, err = m.actionStore.Put(ctx, key, actionDefinition)
+		if err != nil {
+			log.Warn(err)
+			return nil, err
+		}
+
+		e2smKpmActionDefinition, err := pdubuilder.CreateE2SmKpmActionDefinitionFormat1(reportStyle.Type, actionDefinition)
+		if err != nil {
+			return nil, err
+		}
+
+		e2smKpmActionDefinitionProto, err := proto.Marshal(e2smKpmActionDefinition)
+		if err != nil {
+			return nil, err
+		}
+
+		action := &e2api.Action{
+			ID:   int32(index),
+			Type: e2api.ActionType_ACTION_TYPE_REPORT,
+			SubsequentAction: &e2api.SubsequentAction{
+				Type:       e2api.SubsequentActionType_SUBSEQUENT_ACTION_TYPE_CONTINUE,
+				TimeToWait: e2api.TimeToWait_TIME_TO_WAIT_ZERO,
+			},
+			Payload: e2smKpmActionDefinitionProto,
+		}
+
+		actions = append(actions, *action)
 
 	}
 	return actions, nil
